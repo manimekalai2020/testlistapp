396694ec167dd509c20ddf22692e593143fc960f
manimekalai2020 <60381471+manimekalai2020@users.noreply.github.com>
2024-02-23 14:36:30 +0530

    Create country.dart

---
+++ lib/feature/list/domain/entities/country.dart
@@ -0,0 +1,7 @@
+class Country {
+  final String countryCode;
+  final String countryName;
+  final String region;
+
+  Country({required this.countryCode, required this.countryName, required this.region});
+}
